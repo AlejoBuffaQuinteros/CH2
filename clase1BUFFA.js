fe114347bcf5c4bbbc22fa50c68a9f4fbed6f880
AlejoBuffaQuinteros <82730143+AlejoBuffaQuinteros@users.noreply.github.com>
2021-04-17 17:08:31 -0300

    Create clase1BUFFA.js

---
+++ clase1BUFFA.js
@@ -0,0 +1,20 @@
+let palabra = prompt('ingresa una palabra') 
+
+ let hola = "hola" ==  palabra
+ let HOLA = "HOLA" == palabra
+ let Hola = "Hola" == palabra
+
+ if ( hola || HOLA || Hola ) { 
+     console.log ('HOLA!')}
+     else{
+        alert('EEROR: Escriba HOLA')}
+    
+
+ 
+ let numero= prompt ('ingresar numero')
+
+     if (numero > 1000)
+     alert ('el numero es mayor a 1000')
+     else (numero < 1000)
+     alert ('el numero es menor a 1000')
+
